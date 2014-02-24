d6c45c095803ef0c6b0cb76e5242aa31990f1f8f
Sting George Arroyo Hernández <stingarroyo@gmail.com>
2014-02-24 18:23:36 -0500

    Agregando imagen
    Se inserta imagen descriptiva de lo que esta representando la carga
    progresiva, esta imagen por el momento es de un tamaño fijo de 100 x 100
    px

--- js/cargador-procedimental.jquery.js
+++ js/cargador-procedimental.jquery.js
@@ -10,6 +10,11 @@
             var ctx = lienzo.getContext('2d'),
             alto = lienzo.height,
             ancho = lienzo.width;
+            var img = new Image(100, 100);
+            img.src = 'img/html5.png';
+            img.onload = function(){
+               ctx.drawImage(img, ancho/2 - 50, 170);
+            }
             $('a').on('click',function(e){
                e.preventDefault();
                enProceso++;
@@ -19,7 +24,7 @@
                console.log(enProceso);
             });
          }
-         enProceso = 0;//falta actualizar este valor
+         enProceso = 0;//falta actualizar este valor para volver a empezar la funcion
          function carga(ini, porc){
             setInterval(function(){
                if(ini <= porc){
@@ -32,13 +37,14 @@
          }
          function dib_cir(porc){
             ctx.clearRect(0, 0, ancho, alto);
+            ctx.drawImage(img, ancho/2 - 50, 170);
             var ang = (360*porc)/100;
             //ciculo externo
             ctx.beginPath();
             ctx.fillStyle = bord;
             //derecha a izquierda
-            ctx.arc(ancho/2,alto/2,75,(Math.PI/-2),(Math.PI/180*ang)-(Math.PI/2));
-            ctx.arc(ancho/2,alto/2,10,(Math.PI/180*ang)-(Math.PI/2),(Math.PI/-2));
+            ctx.arc(ancho/2,80,75,(Math.PI/-2),(Math.PI/180*ang)-(Math.PI/2));
+            ctx.arc(ancho/2,80,10,(Math.PI/180*ang)-(Math.PI/2),(Math.PI/-2));
             //izquierda a derecha
             /*ctx.arc(100,100,75,(Math.PI/-2),(Math.PI/-2)+(Math.PI/180*-ang),true);
             ctx.arc(100,100,10,(Math.PI/-2)+(Math.PI/180*-ang),(Math.PI/-2),true);*/
@@ -47,14 +53,14 @@
             //ciculo medio
             ctx.beginPath();
             ctx.fillStyle = rell;
-            ctx.arc(ancho/2,alto/2,65,0,Math.PI/180*360);
+            ctx.arc(ancho/2,80,65,0,Math.PI/180*360);
             ctx.closePath();
             ctx.fill();
             //circulo interno
             ctx.beginPath();
             ctx.strokeStyle = txt;
             ctx.lineWidth = 5;
-            ctx.arc(ancho/2,alto/2,60,0,Math.PI/180*360);
+            ctx.arc(ancho/2,80,60,0,Math.PI/180*360);
             ctx.closePath();
             ctx.stroke();
             //texto porcentaje
@@ -62,7 +68,7 @@
             ctx.font = '40px Open Sans';
             ctx.textAlign = 'center';
             ctx.textBaseline = 'middle';
-            ctx.fillText(porc+'%',ancho/2, alto/2);
+            ctx.fillText(porc+'%',ancho/2, 80);
          }
       });
    }
