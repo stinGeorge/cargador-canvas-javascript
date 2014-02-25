359586c24d17aab7afd25be09098393cebe84b12
Sting George Arroyo Hernández <stingarroyo@gmail.com>
2014-02-25 13:11:22 -0500

    Agregando comentarios
    Hemos añadido comentarios al código para una mayor comprensión de todo
    el proceso realizado.

--- js/cargador-procedimental.jquery.js
+++ js/cargador-procedimental.jquery.js
@@ -1,49 +1,66 @@
 ;(function($, window, undefined){
+   //Usamos javascript estricto para tener buenas practicas al codear ;D
    'use strict';
+   //Definimos la funciónel del plguin para que reciba parámetros de personalización básica
    $.fn.cargador = function(ini, porc, bord, rell, txt, url, time){
+      //Iniciamos un loop para que se aplique a todo el array de objetos jQuery
       return this.each(function(){
+         //Seleccionamos el objeto al cual se aplico el método
          var $lienzo = $(this),
          lienzo = $lienzo[0];
+         //Revisamos si el navegador soporta canvas para no dibujar innecesariamente
          if(lienzo.getContext){
+            //Obtenemos el contexto del elemento seleccionado
             var ctx = lienzo.getContext('2d'),
+            //Definimos las variables necesarias para la función
             alto = lienzo.height,
-            ancho = lienzo.width;
-            var animacion = false;
-            var img = new Image(100, 100);
+            ancho = lienzo.width,
+            animacion = false,
+            //Creamos una imágen con tamaño fijo y url dinámcia
+            img = new Image(100, 100);
             img.src = url;
+            //Esperamos que el navegador cargue la imagen para renderizarla correctamente en el lienzo
             img.onload = function(){
+               //Ubicacion de la imagen
                ctx.drawImage(img, ancho/2 - 50, 170);
+               //Definimos el evento y el elemento que lo va a activar
+               $('a').on('click',function(e){
+                  e.preventDefault();
+                  //Evitamos que se ejecuten varios eventos al mismo tiempo, sino que espere a que termine uno para iniciar otro
+                  if(!animacion){
+                     animacion = true;
+                     carga(ini, porc);
+                  }
+               });
             }
-            $('a').on('click',function(e){
-               e.preventDefault();
-               if(!animacion){
-                  animacion = true;
-                  carga(ini, porc);
-               }
-            });
          }
+         //Función que se encarga de simular el dibujo del circulo frame por frame
          function carga(ini, porc){
             var ciclo = setInterval(function(){
                if(ini <= porc){
                   dib_cir(ini);
                   ini++;
                }else{
+                  //Detenemos el bucle para poder iniciar otra animación
                   clearInterval(ciclo);
                   animacion = false;
                }
             },time/porc);
          }
+         //Función que dibuja directamente sobre el lienzo con todas sus propiedades
          function dib_cir(porc){
+            //Limpiamos el lienzo para renderizar el sgt. frame
             ctx.clearRect(0, 0, ancho, alto);
             ctx.drawImage(img, ancho/2 - 50, 170);
+            //Obtenemos el angulo preciso para el frame
             var ang = (360*porc)/100;
             //ciculo externo
             ctx.beginPath();
             ctx.fillStyle = bord;
-            //derecha a izquierda
+            //Carga de derecha a izquierda
             ctx.arc(ancho/2,80,75,(Math.PI/-2),(Math.PI/180*ang)-(Math.PI/2));
             ctx.arc(ancho/2,80,10,(Math.PI/180*ang)-(Math.PI/2),(Math.PI/-2));
-            //izquierda a derecha
+            //Carga de izquierda a derecha
             /*ctx.arc(100,80,75,(Math.PI/-2),(Math.PI/-2)+(Math.PI/180*-ang),true);
             ctx.arc(100,80,10,(Math.PI/-2)+(Math.PI/180*-ang),(Math.PI/-2),true);*/
             ctx.closePath();
@@ -61,7 +78,7 @@
             ctx.arc(ancho/2,80,60,0,Math.PI/180*360);
             ctx.closePath();
             ctx.stroke();
-            //texto porcentaje
+            //texto del porcentaje
             ctx.fillStyle = txt;
             ctx.font = '40px Open Sans';
             ctx.textAlign = 'center';
